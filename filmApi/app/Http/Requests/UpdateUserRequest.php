2d3d96ad2518ef7b39b9c3c361aeeeebf3562381
bernadin95 <djebibernadin95@gmail.com>
2025-11-26 23:17:37 -0500

    Les validations des requêtes

---
+++ filmApi/app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'login'      => 'required|string|max:50|unique:users,login',
+            'password'   => 'required|string|min:6',
+            'email'      => 'required|email|max:50|unique:users,email',
+            'last_name'  => 'required|string|max:50',
+            'first_name' => 'required|string|max:50',
+        ];
+    }
+}
