3348b8ba0c8099f57266b0968e60b6dcd190a8c2
bernadin95 <djebibernadin95@gmail.com>
2025-11-26 22:36:33 -0500

    Implémentation des ressources

---
+++ filmApi/app/Http/Resources/FilmResource.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FilmResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'              => $this->id,
+            'title'           => $this->title,
+            'release_year'    => $this->release_year,
+            'length'          => $this->length,
+            'description'     => $this->description,
+            'rating'          => $this->rating,
+            'language_id'     => $this->language_id,
+            'special_features'=> $this->special_features,
+            'image'           => $this->image,
+            'created_at'      => $this->created_at,
+        ];
+    }
+}
+
